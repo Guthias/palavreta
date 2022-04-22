608a2d4275f0fad8b2482a8bf30aba33fb2aadb0
Guthias <gmgustavom@gmail.com>
2022-04-22 19:25:44 -0300

    test: creating test for verify all positions is correct when tried and correct word is the same

---
+++ src/tests/verifyWords.test.js
@@ -0,0 +1,31 @@
+import verifyWords from '../helpers/verifyWords';
+
+describe('Alghoritm for verify Words', () => {
+  it('Should verify if all positions is correct when tried and correct word is the same', () => {
+    const result = verifyWords('VAZIO', 'VAZIO');
+    expect(result).toMatchObject(
+      [
+        {
+          char: 'V',
+          status: 'correct',
+        },
+        {
+          char: 'A',
+          status: 'correct',
+        },
+        {
+          char: 'Z',
+          status: 'correct',
+        },
+        {
+          char: 'I',
+          status: 'correct',
+        },
+        {
+          char: 'O',
+          status: 'correct',
+        },
+      ]
+    )
+  });
+});
