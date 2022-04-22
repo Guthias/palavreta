81e39c391a27ae2e76ac686caf952da63090b4f7
Guthias <gmgustavom@gmail.com>
2022-04-22 20:11:41 -0300

    feat: making verifyWords verify wrong and incorect (present) positons

--- src/helpers/verifyWords.js
+++ src/helpers/verifyWords.js
@@ -2,8 +2,15 @@ export default function verifyWords(triedWord, correctWord) {
   const triedChars = triedWord.split('');
   const correctChars = correctWord.split('');
 
-  return triedChars.map((char, index) => ({
-    char,
-    status: char === correctChars[index] ? 'correct' : 'incorrect',
-  }));
+  return triedChars.map((char, index) => {
+    let status = 'wrong';
+
+    if (char === correctChars[index]) {
+      status = 'correct';
+    } else if (correctChars.includes(char)) {
+      status = 'present';
+    }
+
+    return { char, status };
+  });
 }
