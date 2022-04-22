2fcfb8b141b4d078e19e75e01d1347f09f17f1be
Guthias <gmgustavom@gmail.com>
2022-04-22 20:01:27 -0300

    test: creating test for verify incorrect positons

--- src/tests/verifyWords.test.js
+++ src/tests/verifyWords.test.js
@@ -28,4 +28,30 @@ describe('Alghoritm for verify Words', () => {
       ]
     )
   });
+
+  it('Should work as expected when tried word is \'FOLHA\' and tried word is \'VENTO\'', () => {
+    const result = verifyWords('FOLHA', 'VENTO');
+    expect(result).toMatchObject(
+      {
+        char: 'F',
+        status: 'wrong',
+      },
+      {
+        char: 'O',
+        status: 'present',
+      },
+      {
+        char: 'L',
+        status: 'wrong',
+      },
+      {
+        char: 'H',
+        status: 'wrong',
+      },
+      {
+        char: 'A',
+        status: 'wrong',
+      },
+    )
+  })
 });
