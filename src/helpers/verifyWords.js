0bacc563bb0fdd856d11d4db8d25f95a1415a60c
Guthias <gmgustavom@gmail.com>
2022-04-22 20:35:11 -0300

    fix: fixing verify words for dont mark as correct repeated chars

--- src/helpers/verifyWords.js
+++ src/helpers/verifyWords.js
@@ -2,15 +2,19 @@ export default function verifyWords(triedWord, correctWord) {
   const triedChars = triedWord.split('');
   const correctChars = correctWord.split('');
 
-  return triedChars.map((char, index) => {
+  const wordStatus = triedChars.map((char, index) => {
     let status = 'wrong';
 
     if (char === correctChars[index]) {
       status = 'correct';
-    } else if (correctChars.includes(char)) {
-      status = 'present';
+      correctChars[index] = ' ';
     }
 
     return { char, status };
   });
+
+  return wordStatus.map(({ char, status }) => ({
+    char,
+    status: correctChars.includes(char) ? 'present' : status,
+  }));
 }
