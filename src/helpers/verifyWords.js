a2efa23dda1ddf249fd0c82879dda8357f177141
Guthias <gmgustavom@gmail.com>
2022-04-22 19:35:22 -0300

    feat: updating verifyWords for verify correct positions

--- src/helpers/verifyWords.js
+++ src/helpers/verifyWords.js
@@ -1,24 +1,9 @@
-export default function verifyWords(correctChars, triedChars) {
-  if (!correctChars || !triedChars) return undefined;
+export default function verifyWords(triedWord, correctWord) {
+  const triedChars = triedWord.split('');
+  const correctChars = correctWord.split('');
 
-  const correctCharsClone = [...correctChars];
-  const wordStatus = triedChars.map((char) => ({
+  return correctChars.map((char, index) => ({
     char,
-    status: 'blank',
+    status: char === triedChars[index] ? 'correct' : 'incorrect',
   }));
-
-  correctChars.forEach((correctChar, index) => {
-    if (correctChar === triedChars[index]) {
-      correctCharsClone[index] = ' ';
-      wordStatus[index].status = 'correct';
-    }
-  });
-
-  correctCharsClone.forEach((_, index) => {
-    if (correctCharsClone.includes(triedChars[index])) {
-      wordStatus[index].status = 'present';
-    }
-  });
-
-  return wordStatus;
 }
