6ab41798af11d28e030bb7a5885de11e4e739d5d
Guthias <gmgustavom@gmail.com>
2022-04-22 20:52:38 -0300

    feat: making verify words return blank when triedWord dont exist

--- src/helpers/verifyWords.js
+++ src/helpers/verifyWords.js
@@ -1,6 +1,13 @@
 export default function verifyWords(triedWord, correctWord) {
-  const triedChars = triedWord.split('');
+  const triedChars = triedWord?.split('');
   const correctChars = correctWord.split('');
+
+  if (!triedWord) {
+    return correctChars.map(() => ({
+      char: '',
+      status: 'blank',
+    }));
+  }
 
   const wordStatus = triedChars.map((char, index) => {
     let status = 'wrong';
