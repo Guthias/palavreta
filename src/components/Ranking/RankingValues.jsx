4fc40d29c8d5407f7a24c049bba02f20dbe51591
Gustavo Mathias <gmgustavom@gmail.com>
2022-05-01 14:29:10 -0300

    feat: showing values on ranking

--- src/components/Ranking/RankingValues.jsx
+++ src/components/Ranking/RankingValues.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import useRanking from '../../hooks/useRanking';
 
 const Row = styled.div`
   display: flex;
@@ -29,25 +30,38 @@ const Description = styled.span`
 `;
 
 export default function RankingValues() {
+  const { ranking } = useRanking();
+
+  const playedGames = ranking.length;
+  const percentageOfWins = Math.round(
+    (ranking.filter(({ win }) => win).length / playedGames) * 100,
+  );
+  const streak = ranking.reduce((acc, { win }) => {
+    acc.actual = win ? acc.actual += 1 : 0;
+    acc.best = acc.actual > acc.best ? acc.best = acc.actual : acc.best;
+
+    return acc;
+  }, { actual: 0, best: 0 });
+
   return (
     <Row>
       <ValueArea>
-        <Value>0</Value>
+        <Value>{ playedGames }</Value>
         <Description>jogos</Description>
       </ValueArea>
 
       <ValueArea>
-        <Value>0</Value>
+        <Value>{ `${percentageOfWins}%` }</Value>
         <Description>de vitórias</Description>
       </ValueArea>
 
       <ValueArea>
-        <Value>0</Value>
+        <Value>{ streak.actual }</Value>
         <Description>sequencia de vitórias</Description>
       </ValueArea>
 
       <ValueArea>
-        <Value>0</Value>
+        <Value>{ streak.best }</Value>
         <Description>melhor seqûencia</Description>
       </ValueArea>
     </Row>
--- src/hooks/useRanking.js
+++ src/hooks/useRanking.js
@@ -22,5 +22,5 @@ export default function useRanking() {
     setRanking(newRanking);
   };
 
-  return { addValueOnRanking };
+  return { ranking, addValueOnRanking };
 }
